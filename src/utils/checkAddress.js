5c0c6aa84a7eb02d17a6318923923146fec59a63
Dimple-Kanwar <dimplekanwar.dks@gmail.com>
2024-09-26 16:35:16 +0530

    updated invalid Address

--- src/utils/checkAddress.js
+++ src/utils/checkAddress.js
@@ -2,15 +2,11 @@ import { ethers } from 'ethers';
 
 // Function to check if an address is a contract or wallet
 export async function checkAddressType(address, provider) {
-    console.log({address,provider})
     // Ensure the address is valid
     if (!ethers.isAddress(address)) {
-        console.log('Invalid address format');
-        return "invalidAddress";
+        // console.log('Invalid address format');
+        return "Invalid Address!";
     }
-
-    // Connect to an Ethereum provider (you can use Infura, Alchemy, etc.)
-    // const provider = new ethers.providers.JsonRpcProvider('https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID');
 
     // Get the code at the address
     const code = await provider.getCode(address);
@@ -23,9 +19,3 @@ export async function checkAddressType(address, provider) {
         return "contract";
     }
 }
-
-// Example usage
-// const addressToCheck = '0xYourAddressHere';
-// checkAddressType(addressToCheck)
-//     .then(() => console.log('Check completed'))
-//     .catch((error) => console.error('Error:', error.message));
